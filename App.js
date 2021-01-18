cfd310e4399544b5704909098410d30bdb019fcc
Stav Kidron <kidronstav@gmail.com>
2021-01-18 15:36:29 -0600

    Created custom alert with multiple buttons and console logs

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Button, Alert } from 'react-native';
 
 export default function App() {
 
@@ -20,7 +20,10 @@ export default function App() {
 
       <Button
         title="Click Me!"
-        onPress={() => console.log('Button Tapped')}
+        onPress={() => Alert.alert("My Title", "My Message", [
+          {text: "Yes", onPress: () => console.log("Yes")},
+          {text: "No", onPress: () => console.log("No")},
+        ])}
         color="orange"/>
 
     </SafeAreaView>
