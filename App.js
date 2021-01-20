9278857805ca797848185aefd8a0063c2bd3a14c
Stav Kidron <kidronstav@gmail.com>
2021-01-19 22:21:51 -0600

    view image screen created

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen.js';
+import ViewImageScreen from './app/screens/ViewImageScreen.js'
+import { View } from 'react-native';
 
 export default function App() {
   
   return (
-      <WelcomeScreen/>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+
+      </View>
   );
 }
