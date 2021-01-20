df0d11100d7b933e7ec7ac2696378f3edc1d39eb
Stav Kidron <kidronstav@gmail.com>
2021-01-20 11:50:52 -0600

    added list item

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Image, View, StyleSheet } from 'react-native';
+
 import AppText from '../components/AppText';
 import colors from '../config/colors';
+import ListItem from '../components/ListItem';
 
 function ListingDetailsScreen(props) {
     return (
         <View>
-            <Image style={styles.image} source={require('../assets/chair.jpg')}/>
+            <Image style={styles.image} source={require('../assets/red-jacket.jpg')}/>
             <View style={styles.detailsContainer}>
-                <AppText style={styles.title}>Chair for sale!</AppText>
+                <AppText style={styles.title}>Jacket for sale!</AppText>
                 <AppText style={styles.price}>$100</AppText>
             </View>
+            <View style={styles.userContainer}>
+                <ListItem 
+                    image={require('../assets/Portrait.jpeg')}
+                    title="Stav Kidron"
+                    subTitle="5 Listings"
+                />
+            </View>
+
         </View>
     );
 }
@@ -32,6 +42,9 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: '500',
+    },
+    userContainer: {
+        marginVertical: 40
     }
 })
 
