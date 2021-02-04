782deb36f30714772154e44454b22dbbdd73c4ed
Stav Kidron <kidronstav@gmail.com>
2021-02-03 19:30:41 -0600

    added dynamic icon component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Screen from './app/components/Screen';
 import WelcomeScreen from './app/screens/WelcomeScreen.js';
 import ViewImageScreen from './app/screens/ViewImageScreen.js'
 import AppText from './app/components/AppText';
@@ -8,10 +9,18 @@ import AppButton from './app/components/AppButton.js';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen.js';
 import MessagesScreen from './app/screens/MessagesScreen.js';
+import Icon from './app/components/Icon';
 
 export default function App() {
   
   return (
-      <MessagesScreen/>
+      <Screen>
+        <Icon
+          name="email"
+          size={50}
+          backgroundColor="red"
+          iconColor="white"  
+        />
+      </Screen>
   );
 }
